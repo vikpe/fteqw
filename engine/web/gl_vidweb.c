b9eee6c8bd522cf5ee238767fdd78266967bbe65
Daniel Svensson <dvensson@gmail.com>
2022-06-19 15:27:25 +0200

    Don't set browser title.

--- engine/web/gl_vidweb.c
+++ engine/web/gl_vidweb.c
@@ -350,7 +350,8 @@ qboolean GLVID_ApplyGammaRamps (unsigned int gammarampsize, unsigned short *ramp
 
 void GLVID_SetCaption(const char *text)
 {
-	emscriptenfte_settitle(text);
+	// Let QTube manage title
+	// emscriptenfte_settitle(text);
 }
 
 void Sys_SendKeyEvents(void)
